39f4f805ef489cf703f4b5baf05d4f75d2292e44
AliEl-Zayat <110483901+AliEl-Zayat@users.noreply.github.com>
2022-09-24 23:19:41 +0200

    maps api

--- src/Components/Mapps/Mapps.jsx
+++ src/Components/Mapps/Mapps.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import Map from './imgs/bg.jpg'
+import './Maps.css'
 const Mapps = () => {
   return (
 <section className='maps' id='maps'>
-<img src={Map} className='w-100 img-fluid' alt="maps" />
+<iframe title="This is a unique title" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3629.1627763802035!2d46.8793748153815!3d24.549036463713364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2fa1a044b7faef%3A0x7a62eb6f09a9d9ac!2zS2FmIFRpbGVzIEZhY3Rvcnkg2YXYtdmG2Lkg2YPYp9mBINit2KzYsSDYqNmE2KfYtw!5e0!3m2!1sen!2seg!4v1664050366872!5m2!1sen!2seg" loading="lazy" referrerPolicy="no-referrer-when-downgrade" className='location'></iframe>
+
 </section>
   )
 }
